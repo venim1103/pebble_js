18e5b47a6e4c36d24900ed7c452c50fa6e0b0958
Ari Potinkara <arip1103@gmail.com>
2018-09-21 22:15:42 +0300

    Add files via upload

---
+++ pebble_test.js
@@ -0,0 +1,22 @@
+﻿console.log('Oma demo!');
+
+simply.on('singleClick', function(e) {
+  console.log(util2.format('Yksi clicked $button!', e));
+  simply.subtitle('Pressed ' + e.button + '!');
+});
+
+simply.on('longClick', function(e) {
+  console.log(util2.format('Kauan clicked $button!', e));
+  simply.vibe();
+  simply.scrollable(e.button !== 'select');
+});
+
+simply.on('accelTap', function(e) {
+  console.log(util2.format('Tappasit accel axis $axis $direction!', e));
+  simply.subtitle('Tapped ' + (e.direction > 0 ? '+' : '-') + e.axis + '!');
+});
+
+simply.setText({
+  title: 'Simply Demo!',
+  body: 'This is oma demo. Paina buttons or tap the watch!',
+}, true);
